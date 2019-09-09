0757899a80e3463ced52722ec8b7142099385bb5
Austin Smith <trunk9slamchest@gmail.com>
2019-09-09 20:13:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,9 +1,9 @@
 def help
-puts "I accept the following commands:"
-puts "- help : displays this help message"
-puts "- list : displays a list of songs you can play"
-puts "- play : lets you choose a song to play"
-puts "- exit : exits this program"
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 def list(songs)
@@ -13,26 +13,27 @@ def list(songs)
 end
 
 def play(songs)
-  
+
   msg = "Invalid input, please try again"
+
   puts "Please enter a song name or number:"
-  
+
   u_input = gets.strip
-  
+
   songs.each_with_index { |e, i|
     if u_input == "#{i + 1}" || u_input == "#{songs[i]}"
       msg = "Playing #{songs[i]}"
       break
     end
   }
-  
+
   puts msg
 end
 
 def exit_jukebox
-  
+
   puts "Goodbye"
-  
+
 end
 
 def run(songs)
